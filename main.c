65adbb10d9501bbe7d70fb1e63843c6cfa3d0997
ninoa <audrennino@cy-tech.fr>
2024-04-06 01:51:59 +0200

    Commit

--- main.c
+++ main.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
 
+/*void emodji(){
+
+    int i, j, k = 0;
+
+    puts("\n ___ TABLEAU DECIMAL ___");
+    puts("\n     0 1 2 3 4 5 6 7 8 9");
+    for(i = 0; i < 26; i++) {
+        printf("\n %2d  ", i);
+        for(j = 0; j < 10; j++, k++)
+            printf("%c ", k != 7 && k != 8 && k != 9 && k != 10 && k != 13 && k < 256 ? k : ' ');
+    }
+
+    puts("\n _______________________");
+    return 0;
+}*/
+
+
 int main() {
-    printf("Hello, World!\n");
+    puts("😊\n");
     return 0;
 }
