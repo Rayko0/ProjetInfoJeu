8faed870ea518a46de509538c4e8ae74bd1a1d41
ninoa <audrennino@cy-tech.fr>
2024-04-07 22:07:20 +0200

    rien

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 
 typedef struct { // Create the type Room
     int Dimension[2]; // Dimension l and L of the room
-    char** tab; // tableau 2d de la salle
+    char** Tab2D; // tableau 2d de la salle
     char* Doors; // Coordinates of all the doors in the room
 } Room;
 
@@ -22,6 +22,8 @@ void GenerateNumberOfRoom(int* p){ // Generate a random number of room between 1
     *p = rand() % (MAXROOM+1) + 10;
 }
 
+
+
 void CreateRoom(Room* r) { // Create the dimension of the room randomly
     int l, L;
     L = rand() % 8 + 3; // Generate a random length between 3 and 10
@@ -32,34 +34,34 @@ void CreateRoom(Room* r) { // Create the dimension of the room randomly
     r->Dimension[0] = l; // Store dimensions
     r->Dimension[1] = L;
 
-    r->tab = malloc(l * sizeof(char*));
-    if (r->tab == NULL) {
+    r->Tab2D = malloc(l * sizeof(char*));
+    if (r->Tab2D == NULL) {
         printf("Erreur allocation de mémoire pour les coordonnées\n");
-        exit(1);
+        exit(3);
     }
 
     for (int i = 0; i < l; i++) { // Allocation de mémoire pour le tableau 2D
-        r->tab[i] = malloc(L * sizeof(char));
-        if (r->tab[i] == NULL) {
+        r->Tab2D[i] = malloc(L * sizeof(char));
+        if (r->Tab2D[i] == NULL) {
             printf("Erreur allocation de mémoire pour les coordonnées\n");
             exit(1);
         }
 
         for (int j = 0; j < L; j++) {
             if (i == 0 && j == 0) {
-                r->tab[i][j] = '1';
+                r->Tab2D[i][j] = '1';
             } else if (i == 0 && j == L - 1) {
-                r->tab[i][j] = '2';
+                r->Tab2D[i][j] = '2';
             } else if (i == l - 1 && j == 0) {
-                r->tab[i][j] = '3';
+                r->Tab2D[i][j] = '3';
             } else if (i == l - 1 && j == L - 1) {
-                r->tab[i][j] = '4';
+                r->Tab2D[i][j] = '4';
             } else if (i == 0 || i == l - 1) {
-                r->tab[i][j] = '-';
+                r->Tab2D[i][j] = '-';
             } else if (j == 0 || j == L - 1) {
-                r->tab[i][j] = '|';
+                r->Tab2D[i][j] = '|';
             } else {
-                r->tab[i][j] = ' ';
+                r->Tab2D[i][j] = ' ';
             }
         }
     }
@@ -68,7 +70,7 @@ void CreateRoom(Room* r) { // Create the dimension of the room randomly
 void PrintfRoom(Room room){
     for (int i = 0; i < room.Dimension[0]; i++) {
         for (int j = 0; j < room.Dimension[1]; j++) {
-            printf("%c", room.tab[i][j]);
+            printf("%c", room.Tab2D[i][j]);
         }
         printf("\n");
     }
