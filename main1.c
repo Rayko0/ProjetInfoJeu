30b1aaeb3050d33de29999816319adcc7e366578
Timothé <166253916+TimKiCode@users.noreply.github.com>
2024-05-15 19:15:42 +0200

    Update main1.c

--- main1.c
+++ main1.c
@@ -2,38 +2,14 @@
 #include <stdlib.h>
 #include <time.h>
 #include <string.h>
+#include "menuInGame.h"
 #include "fonction.h"
-
+#include "menu.h"
 #define MAXROOM 20
 #define MAX_NAME_LENGTH 5
 
 int main() {
-    srand(time(NULL));
-    printf("\033c");
-    int NumberOfRoom = GenerateNumberOfRoom();
-    printf("Nombre de salle dans la partie : %d\n", NumberOfRoom);
-    Room* World = malloc(sizeof(Room)*NumberOfRoom);
-    Room room;
-    room = CreateFirstRoom();
-    World[0] = room;
-    Player player;
-    player = BuildPlayer();
-    player.room=&World[0];
-    int x = 0;
-    int y = 0;
-    GetMiddle(&x,&y,room);
-    //printf("Le milieu de la salle est [%d][%d]\n", y, x);
-    room.Tab2D[y][x]=player.skin;
-    player.Position.x=x;
-    player.Position.y=y;
-    int cpt=0;
-    PrintfRoom(&player);
-    do{
-        Travel(&player,&World,&cpt);
-        PrintfRoom(&player);
-
-    }while (cpt<NumberOfRoom);
-
+    menu();
 
     return 0;
 }
