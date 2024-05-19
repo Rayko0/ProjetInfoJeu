5f0018bda4ff9ebfe7e2db4fe9bd72d9785fb2a0
Timothé <166253916+TimKiCode@users.noreply.github.com>
2024-05-19 20:28:15 +0200

    Update menuInGame.c

--- Jeu2/menuInGame.c
+++ Jeu2/menuInGame.c
@@ -1,4 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "menuInGame.h"
+void clearScreen() {
+    // Cette fonction dépend du système d'exploitation
+    // Sur Windows, utilisez "cls"
+    // Sur Unix/Linux/Mac, utilisez "clear"
+#ifdef _WIN32
+    system("cls");
+#else
+    system("clear");
+#endif
+}
+
 void saveGame(){
 	printf("ici ça save de malade\n");
 	exit(0);
@@ -8,8 +21,15 @@ void leaveGame(){
 	exit(0);
 }
 void menuInGame(){
-	printf("Sauvegarder et quitter : 1\n");
-	printf("Quitter la partie : 0\n");
+    clearScreen();
+    printf("=========================================\n");
+    printf("|            Menu En jeu                |\n");
+    printf("=========================================\n");
+    printf("| 1. Sauvegarder                        |\n");
+    printf("| 2. Quitter                            |\n");
+    printf("=========================================\n");
+    printf("Veuillez choisir une option : ");
+
 	int choice;
 	scanf("%d", &choice);
 	switch (choice) {
