7786daf5c88d1003abc683de1389140609c6214e
Timothé <166253916+TimKiCode@users.noreply.github.com>
2024-05-21 21:08:49 +0200

    Update fonction.h

--- Jeu2/fonction.h
+++ Jeu2/fonction.h
@@ -4,10 +4,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <stdbool.h>
 #include <string.h>
 
 #define MAXROOM 20
 #define MAX_NAME_LENGTH 5
+#define MAX_WORLD_SIZE 1024
 #define CameraRangeX 15
 #define CameraRangeY 40
 
@@ -27,13 +29,6 @@ typedef struct {
 } Item;
 
 typedef struct {
-    Coordinates position;
-    int DoorIndex;
-    int RoomIndex;
-    int NextRoomIndex;
-} Door;
-
-typedef struct {
     char* skin;
     int exist;
     float Hp;
@@ -42,6 +37,14 @@ typedef struct {
     float Def;
     Coordinates Position;
 } Mob;
+
+
+typedef struct {
+    Coordinates position;
+    int DoorIndex;
+    int RoomIndex;
+    int NextRoomIndex;
+} Door;
 
 typedef struct {
     Coordinates size;
@@ -57,6 +60,9 @@ typedef struct {
 typedef struct {
     Coordinates size;
     char*** map;
+    int NbMax;
+    int NbRooms;
+    int NbDoors;
     Room** rooms;
 } World;
 
@@ -78,19 +84,24 @@ typedef struct {
 
 void vide_buffer();
 void AddRoomToWorld(World* world, Room* room);
+bool isSpaceFree(World* world, Coordinates pos, int k);
+void AddDoorToRoom(World* world, Room* r, int ObDoor);
+void AddRoomToWorld(World* world, Room* room);
 World* CreateWorld(int NumberOfRoom);
 Mob BuildMob();
 Player* BuildPlayer();
 int GenerateNumberOfRoom();
-Room* CreateRoom(int cnt);
-Room* CreateFirstRoom();
+Room* CreateRoom(World* world,int cnt);
+Room* CreateFirstRoom(World* world);
+void PrintMessage(char* message);
 void PrintfRoom(Player* P1, World* world);
 void GetMiddle(int* x, int* y, Coordinates room);
 int findDoor(Player* P1);
+void addToInventory(Player *P1, Room * room);
 void roomCreationInGame(Player* P1, World* wrld, int cpt);
 void doorInteraction(Player* P1, World* World, int* cnt, int dir);
 void combat(Player *player, Mob *mob,World* world);
-void addToInventory(Player *P1, Room * room);
+
 void Travel(Player* P1, World* World, int* cpt);
 
 #endif
