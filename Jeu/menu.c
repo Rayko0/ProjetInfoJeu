1a8a3b4c451ee1d0baf914db0b06b9aa9b535334
Rayko0 <166178255+Rayko0@users.noreply.github.com>
2024-05-22 23:24:04 +0200

    Update menu.c

--- Jeu/menu.c
+++ Jeu/menu.c
@@ -1,66 +1,62 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "menuInGame.h"
-#include "fonction.h"
-#include "menu.h"
 
-void nouvellePartie(){
-    srand(time(NULL));
-    printf("\033c");
-    int NumberOfRoom = GenerateNumberOfRoom();
-    int nbdoor = NumberOfRoom -1;
-    printf("Nombre de salle dans la partie : %d\n", NumberOfRoom);
-    World* world = CreateWorld(NumberOfRoom);
-
-    Room* room = CreateFirstRoom(&nbdoor);
-    //add the room to the array
-    world->rooms[0] = room;
-    Player* player;
-    player = BuildPlayer();
-    player->room= world->rooms[0]; 
-    int x = 0, y =0;
-    //find the coordinates of the player (define them in the middle of the map)
-    GetMiddle(&x,&y,world->size);
-    //change the player coordinates
-    player->Position.x=x;
-    player->Position.y=y;
-    //define the position of the room in the map (we always save the top left corner)
-    room->position.x = x-room->size.x/2;
-    room->position.y = y-room->size.y/2;
-    //copy the room in the map
-    AddRoomToWorld(world, room);
-
-    int cpt=0;
-    while(1){
-        Travel(player,world,&cpt);
+void vide_buffer(){
+    while (getchar()!='\n'){
     }
 }
 
-void chargerPartie(){
-    printf("En construction.\n");
+
+void saveGame(GameState GameState){
+	FILE *file;
+file = fopen("savegame.bin", "wb");
+if (file == NULL) {
+    perror("Erreur lors de l'ouverture du fichier");
+    exit(1);
+}
+size_t written = fwrite(&GameState, sizeof(GameState), 1, file);
+if (written != 1) {
+    perror("Erreur lors de l'écriture dans le fichier");
+}
+fclose(file);
+	exit(0);
 }
 
-void menu(){
-    printf("\033c");
-    int verif;
-    printf("=========================================\n");
-    printf("|            Menu Principal             |\n");
-    printf("=========================================\n");
-    printf("| 1. Nouvelle Partie                    |\n");
-    printf("| 2. Charger Partie                     |\n");
-    printf("| 3. Quitter                            |\n");
-    printf("=========================================\n");
-    printf("Veuillez choisir une option : ");
-    int choice;
-    scanf("%d",&choice);
-    switch (choice) {
-        case 1:
-            nouvellePartie();
-            break;
-        case 2:
-            chargerPartie();
-            break;
-        default:
-            exit(0);
-    }
+void leaveGame(){
+	exit(0);
+}
+
+void menuInGame(GameState GameState){
+
+	int choice;
+	int verif;
+	do{
+		printf("\033c");
+    
+    		printf("\033[33m┌"
+    		"───────────────────"
+    		"┐\033[0m"
+    		"\n");
+    		printf("\033[33m│  Menu en jeu      │\033[0m\n"
+    		"\033[33m│\033[0m  Sauvergarder : 1 \033[33m│\033[0m\n"
+    		"\033[33m│\033[0m  Quitter : 0      \033[33m│\033[0m\n");
+    		printf("\033[33m└"
+    		"───────────────────"
+    		"┘\033[0m"
+    		"\n\n");
+		printf("Veuillez choisir une option : ");
+
+		verif=scanf("%d", &choice);
+        	vide_buffer();
+	} while (choice!=1 && choice!=0 && verif !=-1);
+
+	switch (choice) {
+		case 1 :
+			saveGame(GameState);
+			break;
+		default :
+			exit(0);
+
+	}
 }
