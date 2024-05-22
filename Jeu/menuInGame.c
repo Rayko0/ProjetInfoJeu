1f2a72fd137ac51e4bb10617198560d997f88359
Rayko0 <166178255+Rayko0@users.noreply.github.com>
2024-05-22 23:25:19 +0200

    Update menuInGame.c

--- Jeu/menuInGame.c
+++ Jeu/menuInGame.c
@@ -1,20 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "menuInGame.h"
-void saveGame(){
-	printf("ici ça save de malade\n");
+
+void vide_buffer(){
+    while (getchar()!='\n'){
+    }
+}
+
+
+void saveGame(GameState GameState){
+	FILE *file;
+file = fopen("savegame.bin", "wb");
+if (file == NULL) {
+    perror("Erreur lors de l'ouverture du fichier");
+    exit(1);
+}
+size_t written = fwrite(&GameState, sizeof(GameState), 1, file);
+if (written != 1) {
+    perror("Erreur lors de l'écriture dans le fichier");
+}
+fclose(file);
 	exit(0);
 }
 
 void leaveGame(){
 	exit(0);
 }
-void menuInGame(){
-	printf("Sauvegarder et quitter : 1\n");
-	printf("Quitter la partie : 0\n");
+
+void menuInGame(GameState GameState){
+
 	int choice;
-	scanf("%d", &choice);
+	int verif;
+	do{
+		printf("\033c");
+    
+    		printf("\033[33m┌"
+    		"───────────────────"
+    		"┐\033[0m"
+    		"\n");
+    		printf("\033[33m│  Menu en jeu      │\033[0m\n"
+    		"\033[33m│\033[0m  Sauvergarder : 1 \033[33m│\033[0m\n"
+    		"\033[33m│\033[0m  Quitter : 0      \033[33m│\033[0m\n");
+    		printf("\033[33m└"
+    		"───────────────────"
+    		"┘\033[0m"
+    		"\n\n");
+		printf("Veuillez choisir une option : ");
+
+		verif=scanf("%d", &choice);
+        	vide_buffer();
+	} while (choice!=1 && choice!=0 && verif !=-1);
+
 	switch (choice) {
 		case 1 :
-			saveGame();
+			saveGame(GameState);
 			break;
 		default :
 			exit(0);
