c713a00fe8e41dae7833aceb6faf0743790c26e5
Rayko0 <166178255+Rayko0@users.noreply.github.com>
2024-05-22 23:23:52 +0200

    Update fonction.h

--- Jeu/fonction.h
+++ Jeu/fonction.h
@@ -4,94 +4,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <stdbool.h>
 #include <string.h>
-#include <unistd.h>
 
 #define MAXROOM 20
-#define MAX_NAME_LENGTH 7
+#define MAX_NAME_LENGTH 5
+#define MAX_WORLD_SIZE 1024
 #define CameraRangeX 15
 #define CameraRangeY 40
 
 
-typedef struct {
-    int x;
-    int y;
-} Coordinates;
-
-typedef struct {
-   Coordinates position;
-   int exist;
-   int Sword;
-   int Heart;
-   int Shield;
-   char* skin;
-} Item;
-
-typedef struct {
-    Coordinates position;
-    int DoorIndex;
-    int RoomIndex;
-    int NextRoomIndex;
-} Door;
-
-typedef struct {
-    char* skin;
-    int exist;
-    float Hp;
-    float Atk;
-    float Esq;
-    float Def;
-    Coordinates Position;
-} Mob;
-
-typedef struct {
-    Coordinates size;
-    Coordinates position;
-    char*** Tab2D;
-    Door TabDoor[4];
-    Door* TabConnectedDoor[4];
-    int RoomIndex;
-    Mob RoomMob;
-    Item RoomItem;
-} Room;
-
-typedef struct {
-    Coordinates size;
-    char*** map;
-    Room** rooms;
-} World;
-
-typedef struct {
-    char Name[MAX_NAME_LENGTH];
-    float Hp;
-    float Atk;
-    float Def;
-    float Esq;
-    Coordinates Position;
-    char* skin;
-    Room* room;
-    float Exp;
-    Item Inventory[4];
-    int KillCounter;
-    int DeathCounter;
-} Player;
-
-void wait_seconds(double seconds);
-void vide_buffer();
+void AddRoomToWorld(World* world, Room* room);
+bool isSpaceFree(World* world, Coordinates pos, int k);
+void AddDoorToRoom(World* world, Room* r, int ObDoor);
 void AddRoomToWorld(World* world, Room* room);
 World* CreateWorld(int NumberOfRoom);
 Mob BuildMob();
 Player* BuildPlayer();
 int GenerateNumberOfRoom();
-Room* CreateRoom(int cnt);
-Room* CreateFirstRoom();
+Room* CreateRoom(World* world);
+Room* CreateFirstRoom(World* world);
+void PrintMessage(char* message);
 void PrintfRoom(Player* P1, World* world);
 void GetMiddle(int* x, int* y, Coordinates room);
 int findDoor(Player* P1);
-void roomCreationInGame(Player* P1, World* wrld, int cpt);
-void doorInteraction(Player* P1, World* World, int* cnt, int dir);
-void combat(Player *player, Mob *mob,World* world);
 void addToInventory(Player *P1, Room * room);
-void Travel(Player* P1, World* World, int* cpt);
+void roomCreationInGame(Player* P1, World* wrld);
+void doorInteraction(Player* P1, World* World, int dir);
+void combat(Player *player, Mob *mob,World* world);
+
+void Travel(Player* P1, World* World);
 
 #endif
